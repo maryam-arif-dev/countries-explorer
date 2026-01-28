60f0e38291fd65d4b731981ce7250ec8d2dcf753
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-01-28 07:37:00 +0430

    Build initial app layout and UI structure
    - Added main HTML/JSX structure to App.jsx
    - Created header, search, filter, and footer sections
    - Set up layout for countries cards

--- countries-explorer/src/App.jsx
+++ countries-explorer/src/App.jsx
@@ -1,7 +1,67 @@
 import "./App.css";
-
+import { Earth, Search, Funnel, BrushCleaning } from "lucide-react";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="page">
+        {/* Header Section */}
+        <header className="header">
+          <div className="header-title">
+            <span className="header-title-icon">
+              <Earth color="#0f0f0f" />
+            </span>
+            Countries Explorer
+          </div>
+        </header>
+        {/* Search and filter section */}
+        {/* Search Section */}
+        <div className="search-filter-section">
+          <div className="search-column">
+            <span className="search-icon">
+              <Search color="#0f0f0f" />
+            </span>
+            <input type="text" className="search-input" />
+          </div>
+          {/* Filter Section */}
+          <div className="filter-column">
+            <span className="filter-icon">
+              <Funnel color="#0f0f0f" />
+            </span>
+            <select name="" id="">
+              <option value="">
+                <option value="all">All</option>
+                <option value="Africa">Africa</option>
+                <option value="Americas">Americas</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
+              </option>
+            </select>
+          </div>
+          {/* Claer Filter */}
+          <div className="clear-filter-column">
+            <button className="clear-filter">
+              <BrushCleaning color="#0f0f0f" />
+              Claer Filters
+            </button>
+          </div>
+        </div>
+        {/* Countries Card Section */}
+        <div className="countries-cards">
+          <div className="country-card-column">
+            <div className="country-flag"></div>
+            <div className="country-name"></div>
+            <div className="country-region"></div>
+            <div className="country-population"></div>
+          </div>
+        </div>
+        {/* Footer Section */}
+        <footer className="footer">
+          <div className="footer-content">@Maryam Arif</div>
+        </footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
